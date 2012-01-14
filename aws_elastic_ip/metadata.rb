017d1d61046c2c86916ebd827590ceb2b45a69d8
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2012-01-14 17:22:06 +0100

    Moving 'spidermonkey' cookbook onto its own branch

--- aws_elastic_ip/metadata.rb
+++
@@ -1,7 +0,0 @@
-maintainer        "Cloudant"
-maintainer_email  "support@cloudant.com"
-license           "Apache 2.0"
-description       "Configures aws elastic ip"
-version           "0.1"
-long_description  "Configures aws elastic ip"
-depends "heartbeat"
