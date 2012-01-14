a7e6b01d842a506ff7bd95cddf1d2092166b8928
Rafael Durán Castañeda <rafadurancastaneda@gmail.com>
2012-01-14 17:24:22 +0100

    Moving 'libcurl' cookbook onto its own branch

--- libicu/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer        "Cloudant"
-maintainer_email  "support@cloudant.com"
-license           "Apache 2.0"
-description       "Configures libicu"
-version           "0.1"
-long_description  "Configures libicu"
